1c1e5c2cd7337101a3a035ea5010f7dfb6539c0e
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-07-05 16:02:43 +0530

    Hook up speed controls

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -63,7 +63,7 @@ const loopSvg = (color) => (
     `
     <svg xmlns="http://www.w3.org/2000/svg" width='24' height='24' viewBox="0 0 48 48"><title>yt-utils-loopIcon</title>
         <g class="nc-icon-wrapper" fill="${color || '#ffffff'}">
-            <path d="M24 8V2l-8 8 8 8v-6c6.63 0 12 5.37 12 12 0 2.03-.51 3.93-1.39 5.61l2.92 2.92C39.08 30.05 40 27.14 40 24c0-8.84-7.16-16-16-16zm0 28c-6.63 0-12-5.37-12-12 0-2.03.51-3.93 1.39-5.61l-2.92-2.92C8.92 17.95 8 20.86 8 24c0 8.84 7.16 16 16 16v6l8-8-8-8v6z"/>        </g>
+            <path d="M24 8V2l-8 8 8 8v-6c6.63 0 12 5.37 12 12 0 2.03-.51 3.93-1.39 5.61l2.92 2.92C39.08 30.05 40 27.14 40 24c0-8.84-7.16-16-16-16zm0 28c-6.63 0-12-5.37-12-12 0-2.03.51-3.93 1.39-5.61l-2.92-2.92C8.92 17.95 8 20.86 8 24c0 8.84 7.16 16 16 16v6l8-8-8-8v6z"/>
         </g>
     </svg>
     `
@@ -90,13 +90,13 @@ const loopToggleBtn = new Element('div')
 loopToggleBtn.addEventListener('click', () => { videoElement.loop = !videoElement.loop })
 
 //  Observe video element for loop attribute change and change loop-SVG
-const videoObserver = new MutationObserver(mutations => {
+const loopObserver = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
         if (mutation.type !== 'attributes' || mutation.attributeName !== 'loop') { return }
         loopToggleBtn.innerHTML = setLoopHTML()
     })
 })
-videoObserver.observe(videoElement, { attributes: true })
+loopObserver.observe(videoElement, { attributes: true })
 
 //  Position the button in the YouTube LeftControl section before the Volume button (3rd ChildNode)
 youtubeLeftControls.insertBefore(loopToggleBtn, youtubeLeftControls.childNodes[3])
@@ -104,6 +104,36 @@ youtubeLeftControls.insertBefore(loopToggleBtn, youtubeLeftControls.childNodes[3
 //  =============
 //  SPEED CONTROL
 //  =============
+
+//  Speed Control Container Div
+const speedControl = new Element('div')
+    .withID('yt-utils-speedControlArea')
+    .withStyles({
+        display: 'flex',
+        marginLeft: '1rem'
+    })
+    .getElement()
+
+//  Append speedControl area to YouTube LeftControl section
+youtubeLeftControls.append(speedControl)
+
+//  SPEED DISPLAY
+//  =============
+
+//  Displays the current playback rate
+const speedDisplay = new Element('p')
+    .withID('yt-utils-speedDisplay')
+    .withText(videoElement.playbackRate.toString() + 'x')
+    .withStyles({
+        width: '3ch',
+        marginRight: '0.5rem'
+    })
+    .getElement()
+
+speedControl.append(speedDisplay)
+
+//  SPEED RANGE
+//  ===========
 
 //  Range Slider to control speed
 const speedRange = new Element('input')
@@ -115,10 +145,24 @@ const speedRange = new Element('input')
         step: 0.5,
         value: videoElement.playbackRate || 1.0
     })
+    .withStyles({
+        display: 'none',
+        transition: '0.25s ease-in'
+    })
     .getElement()
 
 //  Slider change event listener
 speedRange.addEventListener('change', (e) => { videoElement.playbackRate = e.target.value || 1 })
 
-//  Append slider to YouTube LeftControl section
-youtubeLeftControls.append(speedRange)
+//  Video Element responder
+videoElement.addEventListener('ratechange', (e) => {
+    speedDisplay.innerText = e.target.playbackRate.toString() + 'x'
+    speedRange.value = e.target.playbackRate
+})
+
+//  Hover effects
+speedControl.addEventListener('mouseover', () => { speedRange.style.display = 'block' })
+speedControl.addEventListener('mouseleave', () => { speedRange.style.display = 'none' })
+
+//  Append speedRange to speedControl area
+speedControl.append(speedRange)
