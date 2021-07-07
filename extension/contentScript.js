41e4e3e389d45b760e1e6c3e992d336633656c16
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-07-07 17:27:02 +0530

    Add chevron SVGs

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -82,15 +82,13 @@ function contentScript() {
     //  ===================
 
     //  Returns loop SVG with given color
-    const loopSvg = (color) => (
-        `
-    <svg xmlns="http://www.w3.org/2000/svg" width='24' height='24' viewBox="0 0 48 48"><title>yt-utils-loopIcon</title>
-        <g class="nc-icon-wrapper" fill="${color || '#ffffff'}">
-            <path d="M24 8V2l-8 8 8 8v-6c6.63 0 12 5.37 12 12 0 2.03-.51 3.93-1.39 5.61l2.92 2.92C39.08 30.05 40 27.14 40 24c0-8.84-7.16-16-16-16zm0 28c-6.63 0-12-5.37-12-12 0-2.03.51-3.93 1.39-5.61l-2.92-2.92C8.92 17.95 8 20.86 8 24c0 8.84 7.16 16 16 16v6l8-8-8-8v6z"/>
-        </g>
-    </svg>
+    const loopSvg = (color) => `
+        <svg xmlns="http://www.w3.org/2000/svg" width='24' height='24' viewBox="0 0 48 48"><title>yt-utils-loopIcon</title>
+            <g class="nc-icon-wrapper" fill="${color || '#ffffff'}">
+                <path d="M24 8V2l-8 8 8 8v-6c6.63 0 12 5.37 12 12 0 2.03-.51 3.93-1.39 5.61l2.92 2.92C39.08 30.05 40 27.14 40 24c0-8.84-7.16-16-16-16zm0 28c-6.63 0-12-5.37-12-12 0-2.03.51-3.93 1.39-5.61l-2.92-2.92C8.92 17.95 8 20.86 8 24c0 8.84 7.16 16 16 16v6l8-8-8-8v6z"/>
+            </g>
+        </svg>
     `
-    )
 
     //  Returns loop's innerHTML
     const setLoopHTML = () => videoElement.loop ? loopSvg('#ff0033') : loopSvg()
@@ -132,12 +130,19 @@ function contentScript() {
         .withID('yt-utils-speedControlArea')
         .withStyles({
             display: 'flex',
-            margin: '0 0.5rem'
         })
         .getElement()
 
-    //  Append speedControl area to YouTube LeftControl section before the time element (5th ChlidNode - after being shifted by the previous insertion)
+    //  Append speedControl area to YouTube LeftControl section after the time element (6th ChlidNode - after being shifted by the previous insertion)
     youtubeLeftControls.insertBefore(speedControl, youtubeLeftControls.childNodes[6])
+
+    const setChevron = (color) => `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 48 48"><title>yt-utils-chevron</title>
+            <g class="nc-icon-wrapper" fill=${color || '#ffffff'}>
+                <path d="M20 12l-2.83 2.83L26.34 24l-9.17 9.17L20 36l12-12z"/>
+            </g>
+        </svg>
+    `
 
 
     //      SPEED LEFT CHEVRON
@@ -145,8 +150,14 @@ function contentScript() {
 
     const speedLeftChevron = new DOMElement('div')
         .withID('yt-utils-speedLeftChevron')
-        .withText('-')
-        .withStyles({ opacity: 0 })
+        .withHTML(setChevron())
+        .withStyles({
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            opacity: 0,
+            transform: 'rotate(180deg)'
+        })
         .getElement()
 
     speedLeftChevron.addEventListener('click', () => { if (videoElement.playbackRate > 0.5) { videoElement.playbackRate -= 0.5 } })
@@ -159,8 +170,7 @@ function contentScript() {
     //  Displays the current playback rate
     const speedDisplay = new DOMElement('p')
         .withID('yt-utils-speedDisplay')
-        .withText(videoElement.playbackRate.toString() + 'x')
-        .withStyles({ margin: '0 0.25rem' })
+        .withText(videoElement.playbackRate.toFixed(1) + 'x')
         .getElement()
 
     speedControl.append(speedDisplay)
@@ -171,8 +181,13 @@ function contentScript() {
 
     const speedRightChevron = new DOMElement('div')
         .withID('yt-utils-speedRightChevron')
-        .withText('+')
-        .withStyles({ opacity: 0 })
+        .withHTML(setChevron())
+        .withStyles({
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            opacity: 0
+        })
         .getElement()
 
     speedRightChevron.addEventListener('click', () => { if (videoElement.playbackRate < 4.0) { videoElement.playbackRate += 0.5 } })
