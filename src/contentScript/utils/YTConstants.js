1d1d25dd12d43fedc7ca7daed0fd9bda6ac73e91
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-09-20 15:43:37 +0530

    Add YouTube Player Classes

--- src/contentScript/utils/YTConstants.js
+++ src/contentScript/utils/YTConstants.js
@@ -2,5 +2,6 @@
 //  YOU-TUBE CONSTANTS
 //  ==================
 
+export const ytVideoPlayerClases = ['video-stream', 'html5-main-video'] //  YouTube Player Classes
 export const ytNavEvent = 'yt-navigate-start'    //  YouTube Single-Page-App Navigation Event
 export const ytLeftControls = 'ytp-left-controls'    //  YouTube Player Controls (left-side)
