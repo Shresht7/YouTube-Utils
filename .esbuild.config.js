04824abd0f6a85b17570deb35b28f040c29d6865
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-09-29 13:00:52 +0530

    Add Chokidar File-Watcher

--- .esbuild.config.js
+++ .esbuild.config.js
@@ -1,4 +1,7 @@
-const esbuild = require('esbuild')  //  ESBUILD
+const fs = require('fs')    //  File-System Module
+const path = require('path')    //  Path Module
+const esbuild = require('esbuild')  //  ESBuild
+const chokidar = require('chokidar')    //  Chokidar for file-watch
 
 //  =====================
 //  ESBUILD CONFIGURATION
@@ -23,3 +26,33 @@ const config = {
 //  =============================================================================================
 esbuild.build(config).then(() => console.log('build successful ✅')).catch(() => process.exit(1))
 //  =============================================================================================
+
+//  ==============
+//  CHOKIDAR WATCH
+//  ==============
+
+chokidar.watch('./src').on('all', (event, srcPath, stat) => {
+
+    const trackedFiles = [
+        'manifest.json',
+        'popup.html',
+        'options.html',
+        'icons',
+    ]
+
+    const baseName = path.basename(srcPath)
+    const destPath = path.join(__dirname, 'extension', baseName)
+
+    if (!trackedFiles.includes(baseName)) { return }
+
+    if (stat.isFile()) {
+        fs.copyFile(srcPath, destPath, (err) => { if (err) { console.error(err) } })
+    } else {
+        for (x of fs.readdirSync(srcPath)) {
+            if (!fs.existsSync(destPath)) { fs.mkdirSync(destPath) }
+            fs.copyFile(path.join(srcPath, x), path.join(destPath, x), (err) => { if (err) { console.error(err) } })
+        }
+    }
+
+
+})
