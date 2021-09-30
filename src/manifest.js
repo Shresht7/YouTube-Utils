25e9ec89a8b4d2eb2f492207a6296d3caf925d93
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-09-30 21:18:05 +0530

    Add alternative way to create manifest.json file using js

--- src/manifest.js
+++ src/manifest.js
@@ -1,18 +1,22 @@
-{
+//  ==================
+//  EXTENSION MANIFEST
+//  ==================
+
+module.exports = {
     "name": "YouTube-Utils",
     "description": "A chrome-extension with utility features for YouTube",
     "version": "0.1.0",
     "manifest_version": 3,
-    "action": {
-        "default_popup": "popup.html",
-        "default_icon": {
-            "16": "/icons/yt-utils.png",
-            "32": "/icons/yt-utils@2x.png",
-            "48": "/icons/yt-utils@3x.png",
-            "128": "/icons/yt-utils@8x.png"
-        }
-    },
-    "options_page": "options.html",
+    // "action": {
+    //     "default_popup": "popup.html",
+    //     "default_icon": {
+    //         "16": "/icons/yt-utils.png",
+    //         "32": "/icons/yt-utils@2x.png",
+    //         "48": "/icons/yt-utils@3x.png",
+    //         "128": "/icons/yt-utils@8x.png"
+    //     }
+    // },
+    // "options_page": "options.html",
     "background": {
         "service_worker": "backgroundScript.js"
     },
